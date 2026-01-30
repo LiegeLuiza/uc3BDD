9e371095124bdf992de1e98d322f2ad1abdc2734
LiegeLuiza <camargoliege2@gmail.com>
2026-01-29 21:04:18 -0300

    aula_29-01-2026

---
+++ trabalho_cliente_Produto_Pedido.sql
@@ -0,0 +1,27 @@
+insert into cliente(id_cliente, nome_cliente, sobrenome_cliente)
+Values (1,'liege','luiza');
+
+insert into cliente(id_cliente, nome_cliente, sobrenome_cliente)
+Values (2,'marcelo','oliveira');
+
+insert into cliente(id_cliente, nome_cliente, sobrenome_cliente)
+Values (3,'isadora','vargas');
+
+insert into cliente(id_cliente, nome_cliente, sobrenome_cliente)
+Values (4,'otto','antonio');
+
+
+
+
+
+insert into pedido(id_pedido, id_cliente, id_produto, qtde)
+Values 
+(1,4,2,5),
+(2,3,7,9),
+(3,5,9,8),
+(4,7,5,8);
+
+insert into produto(id_produto, nome_produto, descricao, preco, qtde_estoque, )
+ Values
+ select*from pedido
+ 
