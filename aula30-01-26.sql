ee74c08c1f0b3f22fa16720ab7e704e793de4470
LiegeLuiza <camargoliege2@gmail.com>
2026-01-30 21:02:21 -0300

    Add files via upload

---
+++ aula30-01-26.sql
@@ -0,0 +1,30 @@
+
+ alter table cliente add cpf char(11)
+alter table cliente add salario money
+alter table cliente add genero varchar  
+select*from cliente 
+
+select*from client
+insert into cliente (id_cliente, nome_cliente, sobrenome_cliente)
+values
+
+update cpf 
+set 
+
+
+
+
+where nome_cliente = 'liege'
+
+
+select count (id_cliente) as quantidade_cliente from cliente 
+ select id_cliente, count(id_cliente) from cliente
+ group by id_cliente 
+ having id_cliente>2
+ 
+ 
+ 
+delete from cliente 
+where id_cliente = 1
+
+ 
